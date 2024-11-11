c52704577e3b09cac6ccdf0c2519947da3980b47
Hồ Diệp Huy <hohuy0107@gmail.com>
2024-11-11 16:52:27 +0700

    Note for note.js

--- packages/services/api/src/models/note.js
+++ packages/services/api/src/models/note.js
@@ -1,26 +1,29 @@
-const mongoose = require('mongoose');
+//định nghĩa mô hình Note sử dụng Mongoose để tương tác với cơ sở dữ liệu MongoDB. Mô hình này được sử dụng để lưu trữ thông tin về các ghi chú của người dùng.
+const mongoose = require('mongoose'); // Yêu cầu thư viện Mongoose
 
+// Định nghĩa schema cho tài liệu ghi chú
 const noteSchema = new mongoose.Schema({
     id: {
-        type: String,
-        required: true
+        type: String, // Kiểu dữ liệu là chuỗi
+        required: true // Bắt buộc phải có
     },
     owner: {
-        type: String,
-        required: true
+        type: String, // Kiểu dữ liệu là chuỗi
+        required: true // Bắt buộc phải có
     },
     title: {
-        type: String,
-        required: true,
+        type: String, // Kiểu dữ liệu là chuỗi
+        required: true // Bắt buộc phải có
     },
     content: {
-        type: String,
-        required: true
+        type: String, // Kiểu dữ liệu là chuỗi
+        required: true // Bắt buộc phải có
     },
     createdAt: {
-        type: Date,
-        default: Date.now()
+        type: Date, // Kiểu dữ liệu là ngày tháng
+        default: Date.now() // Giá trị mặc định là thời gian hiện tại
     }
 });
 
+// Xuất mô hình Note để sử dụng trong các phần khác của ứng dụng
 module.exports = new mongoose.model('Note', noteSchema);
