eb39a851d5f07396175f8b4be108dea6450e0ada
Hồ Diệp Huy <hohuy0107@gmail.com>
2024-11-11 16:48:36 +0700

    Note for account.js

--- packages/services/api/src/models/account.js
+++ packages/services/api/src/models/account.js
@@ -1,23 +1,27 @@
-const mongoose = require('mongoose');
+//Tệp này định nghĩa mô hình Note cho các tài liệu ghi chú trong MongoDB. Mô hình này được sử dụng để lưu trữ thông tin ghi chú của người dùng như id, chủ sở hữu, tiêu đề, nội dung và ngày tạo.
 
+const mongoose = require('mongoose'); // Yêu cầu thư viện Mongoose
+
+// Định nghĩa schema cho tài liệu tài khoản
 const accountSchema = new mongoose.Schema({
     name: {
-        type: String,
-        required: true
+        type: String, // Kiểu dữ liệu là chuỗi
+        required: true // Bắt buộc phải có
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+        type: String, // Kiểu dữ liệu là chuỗi
+        required: true, // Bắt buộc phải có
+        unique: true // Phải là duy nhất
     },
     password: {
-        type: String,
-        required: true
+        type: String, 
+        required: true 
     },
     createdAt: {
-        type: Date,
-        default: Date.now()
+        type: Date, // Kiểu dữ liệu là ngày tháng
+        default: Date.now() // Giá trị mặc định là thời gian hiện tại
     }
 });
 
+// Xuất mô hình Account để sử dụng trong các phần khác của ứng dụng
 module.exports = new mongoose.model('Account', accountSchema);
