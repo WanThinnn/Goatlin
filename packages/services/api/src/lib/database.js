060c8a49488d663c58e1d093bab01b8ad118dc17
WanThinnn <thienlai159@gmail.com>
2024-11-12 17:06:36 +0700

    comment for packages/services/api/src/lib/database.js

--- packages/services/api/src/lib/database.js
+++ packages/services/api/src/lib/database.js
@@ -1,12 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); // Nhập thư viện mongoose
 
+// Hàm kết nối tới cơ sở dữ liệu MongoDB
 function connect(connectionString) {
+    // Kết nối tới cơ sở dữ liệu MongoDB sử dụng chuỗi kết nối được cung cấp
     mongoose.connect(connectionString, { useNewUrlParser: true });
 
+    // Lắng nghe sự kiện lỗi kết nối
     mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+    
+    // Lắng nghe sự kiện kết nối thành công
     mongoose.connection.once('open', () => {
         console.log('Database connection established!');
     });
 }
 
+// Xuất hàm connect
 module.exports = { connect };
