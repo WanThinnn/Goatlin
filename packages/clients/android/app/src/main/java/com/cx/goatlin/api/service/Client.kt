e9ed9234e6959a833403975595c5ada90445bc5d
Phucnhii <22521041@gm.uit.edu.vn>
2024-12-09 14:53:05 +0700

    Change https to http

--- packages/clients/android/app/src/main/java/com/cx/goatlin/api/service/Client.kt
+++ packages/clients/android/app/src/main/java/com/cx/goatlin/api/service/Client.kt
@@ -29,64 +29,7 @@
 - Khắc phục: Đảm bảo PreferenceHelper đã được khởi tạo hoặc cung cấp giá trị mặc định trong trường hợp không có dữ liệu.
 
  */
-
 package com.cx.goatlin.api.service
-
-import android.util.Base64
-import com.cx.goatlin.api.model.Account
-import com.cx.goatlin.api.model.Note
-import com.cx.goatlin.helpers.PreferenceHelper
-import retrofit2.Call
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.*
-import okhttp3.CertificatePinner
-import okhttp3.OkHttpClient
-
-interface Client {
-    @POST("accounts")
-    fun signup (@Body data: Account): Call<Void>
-
-    @PUT("accounts/{username}/notes/{note}")
-    fun syncNote(@Header("Authorization") auth: String,
-                 @Path("username") username: String, @Path("note") note: Int,
-                 @Body data: Note): Call<Void>
-
-    companion object {
-        fun getBasicAuthorizationHeader (username: String, password: String): String {
-            val plain: ByteArray = "$username:$password".toByteArray(Charsets.UTF_8)
-            val b64Encoded: String = Base64.encodeToString(plain, Base64.NO_WRAP)
-
-            return "Basic $b64Encoded"
-        }
-
-        fun create(): Client {
-            val hostname: String = PreferenceHelper.getString("ip_address", "127.0.0.1")
-            val port: String = PreferenceHelper.getString("port", "8080")
-            val baseUrl: String = "https://${hostname}:${port}"
-
-            val certificatePinner = CertificatePinner.Builder()
-                .add(baseUrl, "sha256/5Kl14sIBRoArZ8ujwNLWoLOI1QmsvE58nmXTO/9GSJw=")
-                .build()
-
-            val client: OkHttpClient = OkHttpClient.Builder()
-                .certificatePinner(certificatePinner)
-                .build()
-
-
-            val retrofit = Retrofit.Builder()
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create())
-                .baseUrl(baseUrl)
-                .client(client)
-                .build()
-
-            return retrofit.create(Client::class.java)
-        }
-    }
-}
-/*package com.cx.goatlin.api.service
 
 import android.util.Base64
 import com.cx.goatlin.api.model.Account
@@ -129,4 +72,4 @@ interface Client {
             return retrofit.create(Client::class.java)
         }
     }
-}*/
+}
